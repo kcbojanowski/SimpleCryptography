a21f9bc9da5d17052f307f6b485ca3358ddfb0ae
kcbojanowski <kbojanowski@student.agh.edu.pl>
2022-04-20 23:23:40 +0200

    task1/task2

--- Tasks/Task_1.py
+++ Tasks/Task_1.py
@@ -3,6 +3,8 @@ Task no. 1
 Write a code that will encrypt plaintext in Vigenere Cipher. Try to avoid using
 the Internet. You can use given function to generate key.
 """
+
+
 def generateKey(string, key):
     key = list(key)
     string = string.replace(" ", "")
@@ -12,9 +14,12 @@ def generateKey(string, key):
     else:
         for i in range(len(string) - len(key)):
             key.append(key[i % len(key)])
-
     key = "".join(key).upper()
     return key
 
+
+# message is a plain text to encrypt
+# keyword is a key multiplied to match a length of a message
 def Encrypt_Vigenere(message, keyword):
+    # ENTER YOUR CODE HERE
     pass
